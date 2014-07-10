017e995a66996ba4e1715d9e6a8515d10d1cb3a5
pilehvar <pilehvar@pilehvar.di.uniroma1.it>
2014-07-10 15:35:16 +0200

    added JUnit test

---
+++ src/test/java/it/uniroma1/lcl/adw/ADWTest.java
@@ -0,0 +1,238 @@
+package it.uniroma1.lcl.adw;
+
+import static org.junit.Assert.*;
+import it.uniroma1.lcl.adw.comparison.Cosine;
+import it.uniroma1.lcl.adw.comparison.SignatureComparison;
+import it.uniroma1.lcl.adw.comparison.WeightedOverlap;
+
+import org.junit.Test;
+
+public class ADWTest 
+{
+	private static final double epsilon = 0.0001;
+	
+	@Test
+	public void testWeightedOverlap() 
+	{
+        ADW pipeLine = new ADW();
+
+        String text1 = "a mill that is powered by the wind";
+        LexicalItemType text1Type = LexicalItemType.SURFACE;
+        
+        String text2 = "windmill#n rotate#v wind#n";
+        LexicalItemType text2Type = LexicalItemType.SURFACE_TAGGED;
+        
+        String text3 = "windmill.n.1";
+        LexicalItemType text3Type = LexicalItemType.WORD_SENSE;
+        
+        String text4 = "windmill%1:06:01::";
+        LexicalItemType text4Type = LexicalItemType.SENSE_KEYS;
+        
+        String text5 = "terminate";
+        LexicalItemType text5Type = LexicalItemType.SURFACE;
+        
+        String text6 = "fire#v";
+        LexicalItemType text6Type = LexicalItemType.SURFACE_TAGGED;
+        
+        //if lexical items has to be disambiguated
+        DisambiguationMethod disMethod = DisambiguationMethod.ALIGNMENT_BASED;
+        
+        //measure for comparing semantic signatures
+        SignatureComparison measure = new WeightedOverlap(); 
+
+        double score1 = pipeLine.getPairSimilarity(
+                text1, text2,
+                disMethod, 
+                measure,
+                text1Type, text2Type);
+        
+        double score2 = pipeLine.getPairSimilarity(
+                text1, text3,
+                disMethod, 
+                measure,
+                text1Type, text3Type);
+        
+        double score3 = pipeLine.getPairSimilarity(
+                text1, text4,
+                disMethod, 
+                measure,
+                text1Type, text4Type);
+        
+        double score4 = pipeLine.getPairSimilarity(
+                text2, text3,
+                disMethod, 
+                measure,
+                text2Type, text3Type);
+        
+        double score5 = pipeLine.getPairSimilarity(
+                text3, text4,
+                disMethod, 
+                measure,
+                text3Type, text4Type);
+        
+        double score6 = pipeLine.getPairSimilarity(
+                text5, text6,
+                disMethod, 
+                measure,
+                text5Type, text6Type);
+        
+        assertEquals(0.6346, score1, epsilon);
+        assertEquals(0.5624, score2, epsilon);
+        assertEquals(0.5624, score3, epsilon);
+        assertEquals(0.8593, score4, epsilon);
+        assertEquals(1.0, score5, epsilon);
+        assertEquals(1.0, score6, epsilon);
+        
+	}
+	
+	
+	@Test
+	public void testCosine() 
+	{
+        ADW pipeLine = new ADW();
+
+        String text1 = "a mill that is powered by the wind";
+        LexicalItemType text1Type = LexicalItemType.SURFACE;
+        
+        String text2 = "windmill#n rotate#v wind#n";
+        LexicalItemType text2Type = LexicalItemType.SURFACE_TAGGED;
+        
+        String text3 = "windmill.n.1";
+        LexicalItemType text3Type = LexicalItemType.WORD_SENSE;
+        
+        String text4 = "windmill%1:06:01::";
+        LexicalItemType text4Type = LexicalItemType.SENSE_KEYS;
+        
+        String text5 = "terminate";
+        LexicalItemType text5Type = LexicalItemType.SURFACE;
+        
+        String text6 = "fire#v";
+        LexicalItemType text6Type = LexicalItemType.SURFACE_TAGGED;
+        
+        //if lexical items has to be disambiguated
+        DisambiguationMethod disMethod = DisambiguationMethod.ALIGNMENT_BASED;
+        
+        //measure for comparing semantic signatures
+        SignatureComparison measure = new Cosine(); 
+
+        double score1 = pipeLine.getPairSimilarity(
+                text1, text2,
+                disMethod, 
+                measure,
+                text1Type, text2Type);
+        
+        double score2 = pipeLine.getPairSimilarity(
+                text1, text3,
+                disMethod, 
+                measure,
+                text1Type, text3Type);
+        
+        double score3 = pipeLine.getPairSimilarity(
+                text1, text4,
+                disMethod, 
+                measure,
+                text1Type, text4Type);
+        
+        double score4 = pipeLine.getPairSimilarity(
+                text2, text3,
+                disMethod, 
+                measure,
+                text2Type, text3Type);
+        
+        double score5 = pipeLine.getPairSimilarity(
+                text3, text4,
+                disMethod, 
+                measure,
+                text3Type, text4Type);
+        
+        double score6 = pipeLine.getPairSimilarity(
+                text5, text6,
+                disMethod, 
+                measure,
+                text5Type, text6Type);
+        
+        assertEquals(0.5078, score1, epsilon);
+        assertEquals(0.2251, score2, epsilon);
+        assertEquals(0.2251, score3, epsilon);
+        assertEquals(0.6910, score4, epsilon);
+        assertEquals(1.0, score5, epsilon);
+        assertEquals(1.0, score6, epsilon);
+        	
+	}
+	
+	@Test
+	public void testWeightedOverlapWithNoDisambiguation() 
+	{
+        ADW pipeLine = new ADW();
+
+        String text1 = "a mill that is powered by the wind";
+        LexicalItemType text1Type = LexicalItemType.SURFACE;
+        
+        String text2 = "windmill#n rotate#v wind#n";
+        LexicalItemType text2Type = LexicalItemType.SURFACE_TAGGED;
+        
+        String text3 = "windmill.n.1";
+        LexicalItemType text3Type = LexicalItemType.WORD_SENSE;
+        
+        String text4 = "windmill%1:06:01::";
+        LexicalItemType text4Type = LexicalItemType.SENSE_KEYS;
+        
+        String text5 = "terminate";
+        LexicalItemType text5Type = LexicalItemType.SURFACE;
+        
+        String text6 = "fire#v";
+        LexicalItemType text6Type = LexicalItemType.SURFACE_TAGGED;
+
+        
+        //if lexical items has to be disambiguated
+        DisambiguationMethod disMethod = DisambiguationMethod.NONE;
+        
+        //measure for comparing semantic signatures
+        SignatureComparison measure = new WeightedOverlap(); 
+
+        double score1 = pipeLine.getPairSimilarity(
+                text1, text2,
+                disMethod, 
+                measure,
+                text1Type, text2Type);
+        
+        double score2 = pipeLine.getPairSimilarity(
+                text1, text3,
+                disMethod, 
+                measure,
+                text1Type, text3Type);
+        
+        double score3 = pipeLine.getPairSimilarity(
+                text1, text4,
+                disMethod, 
+                measure,
+                text1Type, text4Type);
+        
+        double score4 = pipeLine.getPairSimilarity(
+                text2, text3,
+                disMethod, 
+                measure,
+                text2Type, text3Type);
+        
+        double score5 = pipeLine.getPairSimilarity(
+                text3, text4,
+                disMethod, 
+                measure,
+                text3Type, text4Type);
+        
+        double score6 = pipeLine.getPairSimilarity(
+                text5, text6,
+                disMethod, 
+                measure,
+                text5Type, text6Type);
+        
+        assertEquals(0.76289, score1, epsilon);
+        assertEquals(0.48140, score2, epsilon);
+        assertEquals(0.48140, score3, epsilon);
+        assertEquals(0.59101, score4, epsilon);
+        assertEquals(1.0, score5, epsilon);
+        assertEquals(0.42288, score6, epsilon);
+        
+	}
+	
+}
