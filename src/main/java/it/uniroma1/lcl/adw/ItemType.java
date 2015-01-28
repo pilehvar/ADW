b2f710e1d1666f3f60b0977fc3e0066003a9befc
pilehvar <pilehvar@pilehvar.di.uniroma1.it>
2015-01-28 11:23:48 +0100

    refactored

---
+++ src/main/java/it/uniroma1/lcl/adw/ItemType.java
@@ -0,0 +1,15 @@
+package it.uniroma1.lcl.adw;
+
+/**
+ * the type of the lexical item
+ * @author pilehvar
+ *
+ */
+public enum ItemType 
+{
+	SURFACE,
+	SURFACE_TAGGED,
+	SENSE_OFFSETS,
+	SENSE_KEYS,
+	WORD_SENSE;
+}
